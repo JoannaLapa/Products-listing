305589e879fb9d9ddf0084338f5f1feef7ac0101
Joanna <jlapa.transport@gmail.com>
2023-06-19 17:40:33 +0200

    hard coded functions replaced with forEach

--- main.js
+++ main.js
@@ -3,12 +3,11 @@
 const favBtn1 = document.querySelector('#btn-fav-1')
 const favBtn2 = document.querySelector('#btn-fav-2')
 const favBtn3 = document.querySelector('#btn-fav-3')
+const favbtns = [favBtn1, favBtn2, favBtn3]
 
 const consoleEvent = (e) => {
     console.log('Here should be a logic for adding items to favourite');
     e.target.closest('g').classList.toggle('gold')
 }
 
-favBtn1.addEventListener('click', consoleEvent)
-favBtn2.addEventListener('click', consoleEvent)
-favBtn3.addEventListener('click', consoleEvent)
+favbtns.forEach((btn) => btn.addEventListener('click', consoleEvent))
